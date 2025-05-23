98ec82462fc124e2c87a2de4a4063c65f6ca1cb2
Asher Pope <asher@asherpope.com>
2025-05-23 02:42:33 -0400

    basic blog setup

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,15 @@
-export const schemaTypes = []
+import {authorType} from './authorType'
+import blockContent from './blockContent'
+import {postType} from './postType'
+
+// You will likely need schemas for 'post', 'author', 'category', etc. for the blog later.
+// Import them here when created.
+// import post from './post'
+
+export const schemaTypes = [
+  // Document types
+  authorType,
+  postType,
+  // Object types (used within documents)
+  blockContent,
+]
